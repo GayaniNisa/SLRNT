9f7adef39fba7a4434772e4df3df9d0596533ec8
Gayani Nisansala <gayaninisansala448@gmail.com>
2020-05-30 01:45:54 +0530

    inital commit upto sign in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header/header.component';
+import { SignupComponent } from './signup/signup/signup.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:HeaderComponent},
+  {path: 'signup',component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatCardModule } from '@angular/material/card'
+import { MatInputModule } from '@angular/material/input'
+
+import { HomeLandComponent } from './home/home-land/home-land.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SignupComponent } from './signup/signup/signup.component';
+import { SigninComponent } from './signin/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLandComponent,
+    HeaderComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
